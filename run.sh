9695d263ab3ab3abdede0c0d771652c2bcd9a1f8
df8819 <74317319+df8819@users.noreply.github.com>
2025-07-17 11:07:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-konsole --title="NVIDIA Power Limiter" -e fish -c "cd /home/user1/Documents/GitKraken/NVidiaPowerLimiter; and sudo python3 nvidia_power_limiter.py; and read -P 'Press Enter to close...'"
+konsole --title="NVIDIA Power Limiter" -e fish -c "cd /path/to/NVidiaPowerLimiter; and sudo python3 nvidia_power_limiter.py; and read -P 'Press Enter to close...'"
